d1a89fbf0ff3777fe5d081ced31c3717dd6df61c
Ronny <ronnyd.1994@gmail.com>
2025-02-17 18:55:52 -0500

    ajuste de modals y screen

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -5,122 +5,136 @@ import { AdminContext } from "../context/AdminContex";
 import { useContext, useEffect, useState } from "react";
 import es from "date-fns/locale/es";
 
-
 const locales = { es };
 const localizer = dateFnsLocalizer({
-    format: (date, formatStr) => format(date, formatStr, { locale: es }),
-    parse,
-    startOfWeek: () => startOfWeek(new Date(), { locale: es }),
-    getDay,
-    locales,
+  format: (date, formatStr) => format(date, formatStr, { locale: es }),
+  parse,
+  startOfWeek: () => startOfWeek(new Date(), { locale: es }),
+  getDay,
+  locales,
 });
 
 const MyCalendar = () => {
-    const [selectedEvent, setSelectedEvent] = useState(null);
-    const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
 
+  const { reportAll, userAll } = useContext(AdminContext);
+  const [reports, setReports] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    const { reportAll, userAll } = useContext(AdminContext);
-    const [reports, setReports] = useState([]);
-     const [users, setUsers] = useState([]);
-    
+  useEffect(() => {
+    if (Array.isArray(reportAll) && reportAll.length > 0) {
+      setReports(reportAll);
+    }
+    if (Array.isArray(userAll) && userAll.length > 0) {
+      setUsers(userAll);
+    }
+  }, [reportAll, userAll]);
 
-    useEffect(() => {
-        if (Array.isArray(reportAll) && reportAll.length > 0) {
-            setReports(reportAll);
-        }
-        if (Array.isArray(userAll) && userAll.length > 0) {
-            setUsers(userAll);
-        }
-    }, [reportAll,userAll]);
+  const getUsuarioNombre = (userId) => {
+    const usuario = users?.find((user) => user.id === userId);
+    return usuario ? usuario.nombre : 'Usuario no encontrado'; // Retorna un nombre por defecto si no se encuentra
+  };
 
-    const getUsuarioNombre = (userId) => {
-        const usuario = users?.find((user) => user.id === userId);
-        return usuario ? usuario.nombre : 'Usuario no encontrado'; // Retorna un nombre por defecto si no se encuentra
+  const events = reports?.map((item) => {
+    const startDate = new Date(item.fecha_programada);
+
+    const [hour, minute] = item.hora_programada.split(":");
+    startDate.setHours(hour, minute);
+
+    const endDate = new Date(startDate);
+    endDate.setHours(startDate.getHours() + 1);
+
+    const isSelected = selectedEvent && selectedEvent.title === item.asunto;
+
+    return {
+      title: item.asunto,
+      start: startDate,
+      end: endDate,
+      usuario: getUsuarioNombre(item.usuario_id),
+      descripcion: item.descripcion,
+      tipo: item.tipo,
+      complejidad: item.presupuesto,
+      estado: item.estado,
+      className: isSelected
+        ? 'bg-yellow-400 text-red-500 border-2 border-orange-500' // Fondo amarillo, texto rojo, borde naranja
+        : 'bg-transparent text-black',   // Cuando no está seleccionado, no tiene color de fondo
     };
+  });
 
-    const events = reports?.map((item) => {
-        const startDate = new Date(item.fecha_programada);
+  const handleEventClick = (event, e) => {
+    e.stopPropagation();
 
-        const [hour, minute] = item.hora_programada.split(":");
+    const rect = e.target.getBoundingClientRect();
+    const modalWidth = 300;
+    const modalHeight = 200;
 
-        startDate.setHours(hour, minute);
+    let topPosition = rect.top + window.scrollY + 30;
+    let leftPosition = rect.left + window.scrollX;
 
-        const endDate = new Date(startDate);
-        endDate.setHours(startDate.getHours() + 1);
+    if (leftPosition + modalWidth > window.innerWidth) {
+      leftPosition = window.innerWidth - modalWidth - 20;
+    }
 
-        return {
-            title: item.asunto,
-            start: startDate,
-            end: endDate,
-            usuario: getUsuarioNombre(item.usuario_id), // Aquí agregas el nombre del usuario
-            descripcion: item.descripcion,
-            tipo: item.tipo,
-            complejidad: item.presupuesto,
-            estado: item.estado,
-        };
+    if (topPosition + modalHeight > window.innerHeight) {
+      topPosition = window.innerHeight - modalHeight - 20;
+    }
+
+    setModalPosition({
+      top: topPosition,
+      left: leftPosition,
     });
 
-    const handleEventClick = (event, e) => {
-        e.stopPropagation(); // Evita que el click se propague y cierre el modal inmediatamente
+    setSelectedEvent(event);
+  };
 
-        const rect = e.target.getBoundingClientRect();
-        setModalPosition({
-            top: rect.top + window.scrollY + 30, // Posiciona debajo del evento
-            left: rect.left + window.scrollX,
-        });
+  const closeModal = () => {
+    setSelectedEvent(null);
+  };
 
-        setSelectedEvent(event);
-    };
-
-    const closeModal = () => {
-        setSelectedEvent(null);
-    };
-
-
-    return (
-        <div className="w-full h-full " onClick={closeModal}>
-            <h2>Calendario de Reportes</h2>
-            <Calendar
-                localizer={localizer}
-                events={events}
-                startAccessor="start"
-                endAccessor="end"
-                views={["month", "week", "day"]}
-                defaultView="month"
-                onSelectEvent={handleEventClick}
-
-                messages={{
-                    allDay: 'Todo el día',
-                    previous: 'Anterior',
-                    next: 'Siguiente',
-                    today: 'Hoy',
-                    month: 'Mes',
-                    week: 'Semana',
-                    day: 'Día',
-                    agenda: 'Agenda',
-                    date: 'Fecha',
-                    time: 'Hora',
-                    event: 'Evento',
-                }}
-            />
-            {selectedEvent && (
-                <div
-                    className="absolute z-20 bg-[#4494da] text-white p-2 shadow-md rounded-md border border-gray-300"
-                    style={{ top: modalPosition.top, left: modalPosition.left }}
-                    onClick={(e) => e.stopPropagation()} // Evita que el modal se cierre al hacer clic dentro
-                >
-                    <h3 className="text-lg  font-bold mb-3">Detalles del Reporte</h3>
-                    <p><strong>Usuario:</strong> {selectedEvent.usuario}</p>
-                    <p><strong>Asunto:</strong> {selectedEvent.title}</p>
-                    <p><strong>Descripción:</strong> {selectedEvent.descripcion}</p>
-                    <p><strong>Tipo:</strong> {selectedEvent.tipo}</p>
-                    <p><strong>Complejidad:</strong> {selectedEvent.complejidad}</p>
-                    <p><strong>Estado:</strong> {selectedEvent.estado}</p>
-                </div>
-            )}
+  return (
+    <div className="w-full h-full " onClick={closeModal}>
+      <h2>Calendario de Reportes</h2>
+      <Calendar
+        className="w-full h-screen"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        views={["month", "week", "day"]}
+        defaultView="month"
+        onSelectEvent={handleEventClick}
+        messages={{
+          allDay: 'Todo el día',
+          previous: 'Anterior',
+          next: 'Siguiente',
+          today: 'Hoy',
+          month: 'Mes',
+          week: 'Semana',
+          day: 'Día',
+          agenda: 'Agenda',
+          date: 'Fecha',
+          time: 'Hora',
+          event: 'Evento',
+        }}
+      />
+      {selectedEvent && (
+        <div
+          className="absolute z-20 bg-[#4494da] text-white p-2 shadow-md rounded-md border border-gray-300"
+          style={{ top: modalPosition.top, left: modalPosition.left }}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <h3 className="text-lg  font-bold mb-3">Detalles del Reporte</h3>
+          <p><strong>Usuario:</strong> {selectedEvent.usuario}</p>
+          <p><strong>Asunto:</strong> {selectedEvent.title}</p>
+          <p><strong>Descripción:</strong> {selectedEvent.descripcion}</p>
+          <p><strong>Tipo:</strong> {selectedEvent.tipo}</p>
+          <p><strong>Complejidad:</strong> {selectedEvent.complejidad}</p>
+          <p><strong>Estado:</strong> {selectedEvent.estado}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default MyCalendar;
